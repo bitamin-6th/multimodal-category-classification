4061dee00a1eff649a46208f008de28280b56db6
RRAtELL <77579408+RRAtELL@users.noreply.github.com>
2021-07-28 17:32:50 +0900

    hw
    hw

--- image_features_extraction.py
+++ image_features_extraction.py
@@ -7,7 +7,7 @@ import numpy as np
 import os
 import csv
 
-data = pd.read_csv('data.csv')
+data = pd.read_csv('text.csv')
 img_url = data['img_url'].tolist()
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -38,11 +38,14 @@ def encode_image(N):
             save_image(n, file+path)
             preprocess_image(path+file)
         except:
-            error.append(str(n))
+            image.append(str(n))
 
-        with torch.no_grad():
-            image_features.append(model.encode_image(
-                image[n]).numpy().reshape(-1))
+        try:
+            with torch.no_grad():
+                image_features.append(model.encode_image(
+                    image[n]).numpy().reshape(-1))
+        except:
+            image_features.append(str(n))
 
     print(len(image), len(error), len(image_features))
 
@@ -54,25 +57,44 @@ def encode_image(N):
     return image, error, image_features
 
 
-index_ = []
+encode_image(36468)
 
+img_data = pd.DataFrame(image_features)
+text = pd.read_csv('text.csv', lineterminator='\n')
 
-def concat_dataset(origin, image_features):
-    origin_data = pd.read_csv(origin)
-    origin_data = pd.DataFrame(origin_data)
-    origin_data = origin_data['name']
+image_data = pd.concat(['text', 'img_data'], axis=1)
+image_data.fillna(0, inplace=True)
 
-    for idx, value in enumerate(error):
-        index_.append(int(value))
+image_data['big'] = 0
+image_data['medium'] = 0
+image_data['small'] = 0
 
-    origin_data=origin_data.drop(index=[index_[:]], aixs=0, inplace=True)
-    origin_data.reset_index(inplace=True)
-    origin_data.drop(['index'], axis=1, inplace=True)
+image_data['label'] = image_data['label'].str.split('>')
 
-    image_data = pd.concat([origin_data, pd.DataFrame(image_features)], axis=1)
-    image_data.to_csv('image_feature.csv', header=True)
+for i in range(36468):
+    for j in range(3):
+        if j == 0:
+            image_data['big'][i] = image_data['label'][i][j]
+        elif j == 1:
+            image_data['medium'][i] = image_data['label'][i][j]
+        elif j == 2:
+            image_data['small'][i] = image_data['label'][i][j]
 
-    return image_data
+lst1 = ['29', '여가/생활편의', '묻고 답하기']
+lst2 = ['설명보기', '묻고 답하기', '163', '13', '619', '49', '15', '200', '42',
+        '586', '297', '351', '38', '최대 5% 적립, 무료 시작', '공지사항', '정보 수정요청', '전체상품']
+lst3 = ['최대 5% 적립, 무료 시작', '레이어 팝업 닫기', '설명보기', '정보 수정요청', '전체상품', '30', '8', '3-4인용', '1-2인용', '29', '4-5인용', '9인용 이상', '31', '7',
+        '198', '382', '5', '최저가 사러가기', '7-8인용', '6-7인용', '6', '17', '2-3인용', '1,624', '16', '공지사항', '묻고 답하기', '충전포인트로 결제시', '첫 화면으로', '5-6인용']
 
+for i in range(36468):
+    for j in range(len(lst1)):
+        if data['big'][i] == lst1[j]:
+            data['big'][i] = 'etc'
+    for j in range(len(lst2)):
+        if data['medium'][i] == lst2[j]:
+            data['medium'][i] = 'etc'
+    for j in range(len(lst3)):
+        if data['small'][i] == lst3[j]:
+            data['small'][i] = 'etc'
 
-concat_dataset('data.csv', image_features)
+data.to_csv('final.csv')
